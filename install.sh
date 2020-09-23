3a783fc0865bcd93d5ee86c4b9c4efc56a0cd84e
Parth576 <parthshah576@gmail.com>
2020-09-23 18:56:08 +0530

    added pkglists and installation script

---
+++ install.sh
@@ -0,0 +1,68 @@
+#This script will do the following things: 
+#   1. Install packages and dependencies (i3-gaps, polybar, etc)
+#   2. Copy all dotfiles to correct locations
+
+# Please note that yay is used as the AUR helper to install packages from 
+# AUR. Please comment out those lines for AUR installation if you use a 
+# different AUR helper.
+
+# Installing required packages using pacman 
+pacman -S --needed --noconfirm - < pacman.txt
+
+# Installing required packages from AUR using yay
+yay -S --needed --noconfirm - < aur.txt
+
+# Installing vim-plug (manages vim plugins)
+# Run :PlugInstall in neovim when opened for the first time to install all the plugins
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# i3
+if [ -d "/home/$SUDO_USER/.config/i3" ]
+then
+    cp ./config/i3/config /home/$SUDO_USER/.config/i3/config
+else 
+    mkdir /home/$SUDO_USER/.config/i3
+    cp ./config/i3/config /home/$SUDO_USER/.config/i3/config
+fi
+
+# neovim
+if [ -d "/home/$SUDO_USER/.config/nvim" ]
+then
+    cp ./config/nvim/init.vim /home/$SUDO_USER/.config/nvim/init.vim
+else 
+    mkdir /home/$SUDO_USER/.config/nvim
+    cp ./config/nvim/init.vim /home/$SUDO_USER/.config/nvim/init.vim
+fi
+
+
+# kitty
+if [ -d "/home/$SUDO_USER/.config/kitty" ]
+then
+    cp ./config/kitty/kitty.conf /home/$SUDO_USER/.config/kitty/kitty.conf
+else 
+    mkdir /home/$SUDO_USER/.config/kitty
+    cp ./config/kitty/kitty.conf /home/$SUDO_USER/.config/kitty/kitty.conf
+fi
+
+# polybar
+if [ -d "/home/$SUDO_USER/.config/polybar" ]
+then
+    cp ./config/polybar/* /home/$SUDO_USER/.config/polybar/
+else 
+    cp -R ./config/polybar /home/$SUDO_USER/.config/
+fi
+
+# rofi
+if [ -d "/home/$SUDO_USER/.config/rofi" ]
+then
+    cp ./config/rofi/* /home/$SUDO_USER/.config/rofi/
+else 
+    cp -R ./config/rofi /home/$SUDO_USER/.config/
+fi
+
+# zsh
+cp ./zshrc /home/$SUDO_USER/.zshrc
+
+# wallpaper
+cp ./mountains-1412683.jpg /home/$SUDO_USER/Downloads/mountains-1412683.jpg
